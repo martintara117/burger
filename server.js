dc5e6f1db58a66eec7d1149ad2177aed138cc5ac
Tara Martin <martin.tara117@gmail.com>
2020-11-28 13:21:40 -0500

    added const handlebars to reuire express-handlebars and created an app get to render index

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const handlebars = require(express.handlebars);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
