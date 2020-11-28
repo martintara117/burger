7e266e4d97c6336f6fe2a42e98d482776ca28aee
Tara Martin <martin.tara117@gmail.com>
2020-11-28 13:23:01 -0500

    created an app.engine and app.set for handlebars

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const handlebars = require(express.handlebars);
+const handlebars = require("express-handlebars");
 app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   res.render("index");
